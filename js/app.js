e9f6f9c74c551ecc7836be006e0a68063f3b1d39
OnlyDropped <asheburenkov@yandex.ru>
2022-10-05 17:43:12 +0300

    created new sections

--- js/app.js
+++ js/app.js
@@ -3,3 +3,11 @@ $('.specialist__slider').slick({
   arrows: false, 
   dots: true,
 })
+
+
+const btnMenu = document.querySelector('.header__btn-menu');
+const menu = document.querySelector('.header__menu')
+btnMenu.addEventListener('click', ()=>{
+  btnMenu.classList.toggle('header__btn-menu--active')
+  menu.classList.toggle('header__menu--active')
+})
