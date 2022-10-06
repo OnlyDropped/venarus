e07cfb9b7f65a25a56391a9f71e3ddb3c2d2c8a2
OnlyDropped <asheburenkov@yandex.ru>
2022-10-06 03:06:24 +0300

    add new changes and style new sections

--- js/app.js
+++ js/app.js
@@ -11,3 +11,28 @@ btnMenu.addEventListener('click', ()=>{
   btnMenu.classList.toggle('header__btn-menu--active')
   menu.classList.toggle('header__menu--active')
 })
+
+$('.header__slider').slick({
+  fade: true, 
+  arrows: true, 
+  //dots: true,
+  prevArrow: '<button type="button" class="slick-prev"><img src="img/icons/arrow-prev.svg" alt=""></button>',
+  nextArrow: '<button type="button" class="slick-next"><img src="img/icons/arrow-next.svg" alt=""></button>',
+})
+
+ $('.production__thumb').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  fade: true,
+  arrows: false,
+  focusOnSelect: true,
+  asNavFor: '.production__nav'
+});
+$('.production__nav').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  fade: true,
+  asNavFor: '.production__thumb',
+  prevArrow: '<button type="button" class="slick-prev"><img src="img/icons/arrow-prev.svg" alt=""></button>',
+  nextArrow: '<button type="button" class="slick-next"><img src="img/icons/arrow-next.svg" alt=""></button>',
+});
